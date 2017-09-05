2b2a7001d9778e47b23b4a4d1fc93a22e9139ce2
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-09-05 23:51:43 +1000

    Change scroll top limit to 45

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -27,7 +27,7 @@ function hasScrolled() {
 	
 	// If they scrolled down and are past the navbar, add class .nav-up.
 	// This is necessary so you never see what is "behind" the navbar.
-	if (st <= 25) {
+	if (st <= 45) {
 		// Top of the page
 		$('nav.primary-nav').removeClass('nav-down').removeClass('nav-up').addClass('nav-top');;
 	}
